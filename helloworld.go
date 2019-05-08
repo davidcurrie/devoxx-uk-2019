1952303ddbae798f6074e67220b6d312aa58a23c
David Currie <dcurrie@cloudbees.com>
2019-05-08 11:08:46 +0100

    Support events

--- helloworld.go
+++ helloworld.go
@@ -1,30 +1,53 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Hello world received a request.")
-	target := os.Getenv("TARGET")
-	if target == "" {
-		target = "World"
+	version := os.Getenv("VERSION")
+	var name string
+	if r.Method == "POST" {
+		name = parseBody(r.Body)
 	}
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	if name == "" {
+		name = "World"
+	}
+	msg := fmt.Sprintf("%s: Hello %s!\n", version, name)
+	log.Print(msg)
+	fmt.Fprint(w, msg)
 }
 
 func main() {
-	log.Print("Hello world sample started.")
-
 	http.HandleFunc("/", handler)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
+	log.Printf("Listening on port %q\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+type cloudEvent struct {
+	Data string `json:"data"`
+}
+
+func parseBody(body io.ReadCloser) string {
+	var event cloudEvent
+	if err := json.NewDecoder(body).Decode(&event); err != nil {
+		log.Printf("Failed to decode request body: %v\n", err)
+		return ""
+	}
+	bytes, err := base64.StdEncoding.DecodeString(event.Data)
+	if err != nil {
+		log.Printf("Failed to decode event data: %v\n", err)
+		return ""
+	}
+	return string(bytes)
 }
